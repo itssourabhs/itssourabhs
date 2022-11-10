7c98e8e5af7acee0554b407cece819d5515b666f
Sourabh Suman <sourabh.suman@samarth.ac.in>
2022-11-10 14:03:40 +0530

    asd

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,17 +7,11 @@ export default function Testimonials() {
     <Container title="Testimonials - Sourabh Suman">
       <h1>About Me</h1>
       <Paragraph>
-        Outside of being employed full-time at Vabaco, I sometimes freelance on{" "}
-        <a
-          href={"https://www.upwork.com/freelancers/~01e9d53a899f106f0d"}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Upwork
-        </a>
-        , working mostly on small to medium sized projects.
-        <br />
-        Here are what just a few of my clients had to say:
+        I am a Full Stack Developer with 4+ years of experience in building
+        scalable and maintainable web applications. I have worked with
+        technologies like React, React Native, Node.js, Express.js, MongoDB,
+        PostgreSQL, GraphQL, and AWS.
+        
       </Paragraph>
       <TestimonialList />
     </Container>
